cb06c907b498ea33470aabae5771c7a1aea4b602
T0aD <julien.perez@epsylonia.net>
2012-07-30 00:58:19 +0200

    - automatic naming of lockfile

--- lock.py
+++ lock.py
@@ -3,25 +3,28 @@
 # This module merely creates a lock file in order to lock the execution of a python script
 # (avoids running twice the same script at the same time..)
 import os.path
+import sys
 
 """
 Usage:
 import lock
 
-with lock.lock(__file__):
+with lock.Lock(__file__):
   main program
 """
 
-class lock():
+class Lock():
     path = '/var/tmp'
 
     # Just generate the full sexy path to the lockfile
-    def __init__(self, name):
+    def __init__(self, name = False):
+        # Leet hack if no name was specified:
+        if name is False:
+            name = sys._getframe(1).f_code.co_filename
         self.lockfile = os.path.realpath(os.path.join(self.path, name.replace('.py', '.lock')))
 
     # Create the lockfile and writes the PID in it
     def __enter__(self):
-
         try:
             fd = os.open(self.lockfile, os.O_CREAT | os.O_EXCL | os.O_RDWR)
         except:
@@ -30,12 +33,8 @@ class lock():
             fd.close()
             print("lockfile %s exists with PID %s" % (self.lockfile, pid))
             exit(1)
-        # I .. I dont know how to do that....   :_(
-#        os.write(fd, os.getpid())
+        os.write(fd, bytes(str(os.getpid()), 'ascii'))
         os.close(fd)
-        fd = open(self.lockfile, 'w')
-        fd.write(str(os.getpid()))
-        fd.close()
 
     # Only remove the lockfile when no exception was raised
     def __exit__(self, etype, evalue, traceback):
